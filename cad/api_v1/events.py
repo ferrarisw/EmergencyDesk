dc225212d9679a3a2e50503f5e2bc847501266b7
Davide Ferrari <davideferrari8@gmail.com>
2018-02-22 11:44:56 +0100

    Added JSON decorator
    This decorator replaces all the invocation of jsonify
    at the end of every api.route.
    
    Signed-off-by: Davide Ferrari <davideferrari8@gmail.com>

--- cad/api_v1/events.py
+++ cad/api_v1/events.py
@@ -1,40 +1,46 @@
-from flask import jsonify, request
+from flask import request
 
 from cad import db
 from cad.api_v1 import api
+from cad.decorators import json
 from cad.models import Event
 
 
 @api.route('/events/', methods=['GET'])
+@json
 def get_events():
-    return jsonify({'events': [event.get_url() for event in
-                               Event.query.all()]})
+    return {'events': [event.get_url() for event in
+                       Event.query.all()]}
 
 
 @api.route('/active_events/', methods=['GET'])
+@json
 def get_active_events():
-    return jsonify({'active_events': [event.get_url() for event in
-                                      Event.query.filter_by(active=True).all()]})
+    return {'active_events': [event.get_url() for event in
+                              Event.query.filter_by(active=True).all()]}
 
 
 @api.route('/events/<int:id>', methods=['GET'])
+@json
 def get_event(id):
-    return jsonify(Event.query.get_or_404(id).export_data())
+    return Event.query.get_or_404(id).export_data()
 
 
 @api.route('/events/', methods=['POST'])
+@json
 def new_events():
     events = Event()
     events.import_data(request.json)
     db.session.add(events)
     db.session.commit()
-    return jsonify({}), 201, {'Location': events.get_url()}
+    return {}, 201, {'Location': events.get_url()}
 
 
 @api.route('/events/<int:id>', methods=['PUT'])
+@json
 def edit_events(id):
     events = Event.query.get_or_404(id)
     events.import_data(request.json)
     db.session.add(events)
     db.session.commit()
-    return jsonify({})
+    return {}
--- cad/api_v1/users.py
+++ cad/api_v1/users.py
@@ -1,34 +1,38 @@
-from flask import jsonify, request
+from flask import request
 
 from cad import db
 from cad.api_v1 import api
+from cad.decorators import json
 from cad.models import User
 
 
 @api.route('/users/', methods=['GET'])
+@json
 def get_users():
-    return jsonify({'users': [user.get_url() for user in
-                              User.query.all()]})
+    return {'users': [user.get_url() for user in User.query.all()]}
 
 
 @api.route('/users/<int:id>', methods=['GET'])
+@json
 def get_user(id):
-    return jsonify(User.query.get_or_404(id).export_data())
+    return User.query.get_or_404(id).export_data()
 
 
 @api.route('/users/', methods=['POST'])
+@json
 def new_user():
     user = User()
     user.import_data(request.json)
     db.session.add(user)
     db.session.commit()
-    return jsonify({}), 201, {'Location': user.get_url()}
+    return {}, 201, {'Location': user.get_url()}
 
 
 @api.route('/users/<int:id>', methods=['PUT'])
+@json
 def edit_user(id):
     user = User.query.get_or_404(id)
     user.import_data(request.json)
     db.session.add(user)
     db.session.commit()
-    return jsonify({})
+    return {}
